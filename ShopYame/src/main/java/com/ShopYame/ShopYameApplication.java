5fc7d3b9848552e0385df90c197bd45b30fdd28e
khangkhongnoi <ckhang177@.com>
2022-07-27 10:42:33 +0700

    upload lan 1

---
+++ ShopYame/src/main/java/com/ShopYame/ShopYameApplication.java
@@ -0,0 +1,13 @@
+package com.ShopYame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopYameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopYameApplication.class, args);
+	}
+
+}
---
+++ ShopYame/src/test/java/com/ShopYame/ShopYameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ShopYame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopYameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
